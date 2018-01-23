0b8135735f61eddecd9c73f4909d2a838c500230
Krunoslav Husak <kruno@binel.hr>
2018-01-23 02:46:45 +0100

    Dodana točka u datumu (nakon godine) u prikazu dana.

--- src/App.vue
+++ src/App.vue
@@ -166,7 +166,7 @@ export default {
       for (let i = 1; i <= noOfDays; i++) {
         let day = new Date(this.year, this.month-1, i).getDay();
         if (day != 0 && day != 6 && !skipDays.includes(i)) {
-          this.table += '<tr><td>' + workingDays + '</td><td>' + i +  '.' + this.month + '.' + this.year + '</td>';
+          this.table += '<tr><td>' + workingDays + '</td><td>' + i +  '.' + this.month + '.' + this.year + '.</td>';
           this.table += '<td>' + this.distanceIn + '</td>';
           this.table += '<td>' + this.distanceOut + '</td>';
           this.table += '<td>automobil</td><td></td></tr>';
